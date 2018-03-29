03857cc81255fb0f76b57a4101855cae7f3475d6
Michael Heinisch <venomx411@gmail.com>
2018-03-29 07:57:49 -0500

    set up player objects and game introduction

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,1 +1,22 @@
 "use strict";
+
+let playerOne = {
+	gesture: undefined,
+	name: undefined,
+	score: 0
+};
+
+let playerTwo = {
+	gesture: undefined,
+	name: undefined,
+	score: 0	
+}
+
+alert("Welcome to Rock Paper Scissors!");
+alert("The goal of the game is to be the first player to get two wins, good luck!");
+
+playerOne.name = prompt("Player One, please enter your name:");
+playerTwo.name = prompt("Player Two, please enter your name:");
+
+alert("As a reminder...\nRock crushes Scissors,\nScissors cuts Paper, and\nPaper covers Rock.");
+alert("Good luck!!!");
