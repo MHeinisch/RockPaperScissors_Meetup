a5d2a71e308f78f2f656e0dbb2b7d8979084fba6
Michael Heinisch <venomx411@gmail.com>
2018-04-02 07:49:41 -0500

    game complete, ready for testing

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -20,3 +20,30 @@ playerTwo.name = prompt("Player Two, please enter your name:");
 
 alert("As a reminder...\nRock crushes Scissors,\nScissors cuts Paper, and\nPaper covers Rock.");
 alert("Good luck!!!");
+
+while(playerOne.score < 2 && playerTwo.score < 2) {
+	playerOne.gesture = prompt(playerOne.name + ", enter your move: [rock] [paper] [scissors]").toLowerCase();
+	playerTwo.gesture = prompt(playerOne.name + ", enter your move: [rock] [paper] [scissors]").toLowerCase();
+
+	alert(playerOne.name + " chose " + playerOne.gesture + "\n" + playerTwo.name + " chose " + playerTwo.gesture);
+
+	if(playerOne.gesture === playerTwo.gesture) {
+		alert("Tie");
+	}
+	else if(playerOne.gesture === "rock" && playerTwo.gesture === "scissors" || playerOne.gesture === "paper" && playerTwo.gesture === "rock") {
+		playerOne.score++;
+		alert(playerOne.name + " wins this round!\nThe score is " + playerOne.score + "-" + playerTwo.score);
+	}
+	else {
+		playerTwo.score++;
+		alert(playerTwo.name + " wins this round\nThe score is " + playerOne.score + "-" + playerTwo.score);
+	}
+}
+
+alert("And the big winner is...");
+alert(".....");
+alert("...............");
+
+if(playerOne.score >= 2) {
+	alert(playerOne.name + " is the victor!");
+}
