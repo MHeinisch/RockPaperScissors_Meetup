63901a46230948dcf13e3bb178ed76ddc22ce224
Michael Heinisch <venomx411@gmail.com>
2018-04-02 14:58:27 -0500

    added button to page and started breaking script into functions

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -1,49 +1,66 @@
 "use strict";
 
-let playerOne = {
-	gesture: undefined,
-	name: undefined,
-	score: 0
-};
+function runGame() {
 
-let playerTwo = {
-	gesture: undefined,
-	name: undefined,
-	score: 0	
-}
+	let playerOne = {
+		gesture: undefined,
+		name: undefined,
+		score: 0
+	};
 
-alert("Welcome to Rock Paper Scissors!");
-alert("The goal of the game is to be the first player to get two wins, good luck!");
+	let playerTwo = {
+		gesture: undefined,
+		name: undefined,
+		score: 0	
+	};
 
-playerOne.name = prompt("Player One, please enter your name:");
-playerTwo.name = prompt("Player Two, please enter your name:");
+	displayGameRules();
 
-alert("As a reminder...\nRock crushes Scissors,\nScissors cuts Paper, and\nPaper covers Rock.");
-alert("Good luck!!!");
+	setPlayerName(playerOne);
+	setPlayerName(playerTwo);
 
-while(playerOne.score < 2 && playerTwo.score < 2) {
-	playerOne.gesture = prompt(playerOne.name + ", enter your move: [rock] [paper] [scissors]").toLowerCase();
-	playerTwo.gesture = prompt(playerOne.name + ", enter your move: [rock] [paper] [scissors]").toLowerCase();
+	while(playerOne.score < 2 && playerTwo.score < 2) {
+		playerOne.gesture = prompt(playerOne.name + ", enter your move: [rock] [paper] [scissors]").toLowerCase();
+		playerTwo.gesture = prompt(playerTwo.name + ", enter your move: [rock] [paper] [scissors]").toLowerCase();
 
-	alert(playerOne.name + " chose " + playerOne.gesture + "\n" + playerTwo.name + " chose " + playerTwo.gesture);
+		alert(playerOne.name + " chose " + playerOne.gesture + "\n" + playerTwo.name + " chose " + playerTwo.gesture);
 
-	if(playerOne.gesture === playerTwo.gesture) {
-		alert("Tie");
+		if(playerOne.gesture === playerTwo.gesture) {
+			alert("Tie");
+		}
+		else if(playerOne.gesture === "rock" && playerTwo.gesture === "scissors" || playerOne.gesture === "paper" && playerTwo.gesture === "rock") {
+			playerOne.score++;
+			document.getElementById("p1Score").innerHtml = playerOne.score;
+			alert(playerOne.name + " wins this round!\nThe score is " + playerOne.score + "-" + playerTwo.score);
+		}
+		else {
+			playerTwo.score++;
+			document.getElementById("p2Score").innerHtml = playerTwo.score;
+			alert(playerTwo.name + " wins this round\nThe score is " + playerOne.score + "-" + playerTwo.score);
+		}
 	}
-	else if(playerOne.gesture === "rock" && playerTwo.gesture === "scissors" || playerOne.gesture === "paper" && playerTwo.gesture === "rock") {
-		playerOne.score++;
-		alert(playerOne.name + " wins this round!\nThe score is " + playerOne.score + "-" + playerTwo.score);
+
+	alert("And the big winner is...");
+	alert(".....");
+	alert("...............");
+
+	if(playerOne.score >= 2) {
+		alert(playerOne.name + " is the victor!");
 	}
 	else {
-		playerTwo.score++;
-		alert(playerTwo.name + " wins this round\nThe score is " + playerOne.score + "-" + playerTwo.score);
+		alert(playerTwo.name + " is the victor!");	
 	}
+
 }
 
-alert("And the big winner is...");
-alert(".....");
-alert("...............");
+function displayGameRules() {
+	alert("Welcome to Rock Paper Scissors!");
+	alert("The goal of the game is to be the first player to get two wins, good luck!");
 
-if(playerOne.score >= 2) {
-	alert(playerOne.name + " is the victor!");
+	alert("As a reminder...\nRock crushes Scissors,\nScissors cuts Paper, and\nPaper covers Rock.");
+	alert("Good luck!!!");
+}
+
+function setPlayerName(player) {
+	player.name = prompt("Player, please enter your name:");	
 }
