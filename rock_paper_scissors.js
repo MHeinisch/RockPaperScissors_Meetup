4aac61a16d9719f00fe67bf916276dba2c61564b
Michael Heinisch <venomx411@gmail.com>
2018-04-03 14:37:46 -0500

    adding validation to gesture selection

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -20,36 +20,17 @@ function runGame() {
 	setPlayerName(playerTwo);
 
 	while(playerOne.score < 2 && playerTwo.score < 2) {
-		playerOne.gesture = prompt(playerOne.name + ", enter your move: [rock] [paper] [scissors]").toLowerCase();
-		playerTwo.gesture = prompt(playerTwo.name + ", enter your move: [rock] [paper] [scissors]").toLowerCase();
+
+		setPlayerGesture(playerOne);
+		setPlayerGesture(playerTwo);
 
 		alert(playerOne.name + " chose " + playerOne.gesture + "\n" + playerTwo.name + " chose " + playerTwo.gesture);
 
-		if(playerOne.gesture === playerTwo.gesture) {
-			alert("Tie");
-		}
-		else if(playerOne.gesture === "rock" && playerTwo.gesture === "scissors" || playerOne.gesture === "paper" && playerTwo.gesture === "rock") {
-			playerOne.score++;
-			document.getElementById("p1Score").innerHtml = playerOne.score;
-			alert(playerOne.name + " wins this round!\nThe score is " + playerOne.score + "-" + playerTwo.score);
-		}
-		else {
-			playerTwo.score++;
-			document.getElementById("p2Score").innerHtml = playerTwo.score;
-			alert(playerTwo.name + " wins this round\nThe score is " + playerOne.score + "-" + playerTwo.score);
-		}
+		determineRoundWinner(playerOne, playerTwo);
+
 	}
 
-	alert("And the big winner is...");
-	alert(".....");
-	alert("...............");
-
-	if(playerOne.score >= 2) {
-		alert(playerOne.name + " is the victor!");
-	}
-	else {
-		alert(playerTwo.name + " is the victor!");	
-	}
+	displayGameWinner(playerOne, playerTwo);
 
 }
 
@@ -63,4 +44,52 @@ function displayGameRules() {
 
 function setPlayerName(player) {
 	player.name = prompt("Player, please enter your name:");	
+}
+
+function setPlayerGesture(player) {
+	let isValid = false;
+	do {
+		player.gesture = prompt(player.name + ", enter your move: [rock] [paper] [scissors]").toLowerCase();
+		isValid = validateGestureInput(player.gesture);
+	} while(!isValid);
+}
+
+function determineRoundWinner(playerOne, playerTwo) {
+	if(playerOne.gesture === playerTwo.gesture) {
+		alert("Tie");
+	}
+	else if(playerOne.gesture === "rock" && playerTwo.gesture === "scissors" || playerOne.gesture === "paper" && playerTwo.gesture === "rock") {
+		playerOne.score++;
+		document.getElementById("p1Score").innerHtml = playerOne.score;
+		alert(playerOne.name + " wins this round!\nThe score is " + playerOne.score + "-" + playerTwo.score);
+	}
+	else {
+		playerTwo.score++;
+		document.getElementById("p2Score").innerHtml = playerTwo.score;
+		alert(playerTwo.name + " wins this round\nThe score is " + playerOne.score + "-" + playerTwo.score);
+	}
+}
+
+function displayGameWinner(playerOne, playerTwo) {
+	alert("And the big winner is...");
+	alert(".....");
+	alert("...............");
+	alert(".........................");
+
+	if(playerOne.score >= 2) {
+		alert(playerOne.name + " is the victor!");
+	}
+	else {
+		alert(playerTwo.name + " is the victor!");	
+	}
+}
+
+function validateGestureInput(userInput) {
+	let isValid = false;
+
+	if(userInput === "rock" || userInput === "paper" || userInput === "scissors"){
+		isValid = true;
+	}
+
+	return isValid;
 }
