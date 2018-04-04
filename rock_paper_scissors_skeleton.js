e8541a7dd761f1bd414fdf1a21459d32b0abae3a
Michael Heinisch <venomx411@gmail.com>
2018-04-04 07:56:49 -0500

    created skeleton .js file for attendees

---
+++ rock_paper_scissors_skeleton.js
@@ -0,0 +1,29 @@
+"use strict";
+
+function runGame() {
+
+}
+
+function determineRoundWinner(playerOne, playerTwo) {
+
+}
+
+function displayGameRules() {
+
+}
+
+function displayGameWinner(playerOne, playerTwo) {
+
+}
+
+function setPlayerGesture(player) {
+
+}
+
+function setPlayerName(player) {
+	
+}
+
+function validateGestureInput(userInput) {
+
+}
